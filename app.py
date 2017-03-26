43a979afafa94ca1e89d345be61daf718dbe4450
Jemma <fanjinzhen1@gmail.com>
2017-03-26 18:18:15 +0000

    init

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template, request, redirect
+from bokeh.io import push_notebook, show, output_notebook
+from bokeh.layouts import row
+from bokeh.plotting import figure
+import quandl
+quandl.ApiConfig.api_key = 'Bc6FchAnNSq7zxfjpZGR'
 
 app = Flask(__name__)
 
@@ -9,6 +14,15 @@ def main():
 @app.route('/index')
 def index():
   return render_template('index.html')
+
+def ticker_input():
+  a=request.args.get('a',0,type='str')
+  data = quandl.get_table('WIKI/PRICES', ticker = a)
+  output_notebook()
+  p=figure(title="close price", plot_height=300, plot_width=600)
+  r=p.line(range(30),data.tail(30).close,color="#2222aa",line_width=3)
+  show(p)
+
 
 if __name__ == '__main__':
   app.run(port=33507)
