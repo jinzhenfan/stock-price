a31b1504510baa00ef2ba6ecdd22e9c5b2e34a4e
Jemma <fanjinzhen1@gmail.com>
2017-03-27 04:20:47 +0000

    change6

--- app.py
+++ app.py
@@ -1,35 +1,71 @@
 from flask import Flask, render_template, request, redirect
+
 from bokeh.io import push_notebook, show, output_notebook
+
 from bokeh.layouts import row
+
 from bokeh.plotting import figure
+
 from bokeh.embed import components
 
+from bokeh.util.string import encode_utf8
+
 import quandl
+
 quandl.ApiConfig.api_key = 'Bc6FchAnNSq7zxfjpZGR'
+
+
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
+
 def main():
+
   return redirect('/index')
 
 @app.route('/index')
-def index():
-  return render_template('index.html')
 
-@app.route('/_ticker_input')
-def ticker_input():
-  a=request.args.get('a',0,type='str')
-  print a
-  data = quandl.get_table('WIKI/PRICES', ticker = a)
-  #output_notebook()
-  print (data.head(5))
-  p=figure(title="close price", plot_height=300, plot_width=600)
-  r=p.line(range(30),data.tail(30).close,color="#2222aa",line_width=3)
-  #html = file_html(p, CDN, "my plot")
-  script,div=components(p)
-  return render_template('graph.html',script=script, div=div)
+def index():
+    return render_template('index.html')
+  
+
+@app.route('/graph',methods = ['POST', 'GET'])
+def graph():
+    if request.method == 'POST':
+          #a=request.args.get('a',0,type=str)
+          a = request.form['text']
+          print a
+          data = quandl.get_table('WIKI/PRICES', ticker = a)  
+          #output_notebook()
+          print (data.head(5))
+          p=figure(title="close price", plot_height=300, plot_width=600)
+          r=p.line(range(30),data.tail(30).close,color="#2222aa",line_width=3)
+          #html = file_html(p, CDN, "my plot")
+          script,div=components(p)
+          html = render_template('graph.html',script=script, div=div)
+          return encode_utf8(html) #render_template('index.html')
+
+
+
+#@app.route('/graph')
+#def graph():
+  #data = quandl.get_table('WIKI/PRICES', ticker = 'GOOG')
+  #p=figure(title="close price", plot_height=300, plot_width=600)
+  #r=p.line(range(30),data.tail(30).close,color="#2222aa",line_width=3)
+  #script,div=components(p)
+ # html = render_template('graph.html',script=script, div=div)
+  #return encode_utf8(html) #render_template('index.html')
+
 
 if __name__ == '__main__':
+
   #app.run(port=33507)
-  app.run(host='0.0.0.0')
+
+  app.run(port=33507)
+
+
+
+
